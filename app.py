88800302130dca80b8cd7c345671bfe5b1b92648
Marc GLOAGUEN <marc.gloaguen@gmail.com>
2023-11-23 23:21:26 +0100

    update streamlit

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+
 import streamlit as st
 import pandas as pd
 
@@ -10,8 +12,10 @@ os.environ["OPENAI_API_KEY"] = open("OPENAI_API_KEY", 'r').read()
 
 st.title('Analyse your CSV')
 
-csv = st.file_uploader('Upload your CSV', type='csv')
-if csv:
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if csv := st.file_uploader('Upload your CSV', type='csv'):
     data = pd.read_csv(csv, sep=',')
     chayGPTAgent = create_pandas_dataframe_agent(
         ChatOpenAI(model_name='gpt-4'),
@@ -19,8 +23,18 @@ if csv:
         verbose=True
     )
     st.dataframe(data)
-    prompt = st.text_input("Ask your question :")
-    if prompt:
-        with st.spinner('Wait for it...'):
-            answer = chayGPTAgent.run(prompt)
-            st.markdown(answer)
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+
+        with st.chat_message("human"):
+            st.markdown(prompt)
+        with st.chat_message("ai"):
+            with st.spinner("Waiting...") :
+                answer = chayGPTAgent.run(prompt)
+                st.markdown(answer)
+        st.session_state.messages.append({"role": "human", "content": prompt})
+        st.session_state.messages.append({"role": "ai", "content": answer})
+
